efe40ebed195f0c7e1165767ae5c7d8a5175f7d6
Artem Safin <artemsafin.webdev@gmail.com>
2020-09-01 16:09:36 +0300

    Добавлена папка с собранным приложением

---
+++ build/precache-manifest.4ee197d10560c83a0b8b499f5c923f44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753a46ba8fffda67a11ad249dbe72263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3363592a6a675ce628e",
+    "url": "/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "7a89aaf1fc0ef6344b89",
+    "url": "/static/css/main.a1a8d0fe.chunk.css"
+  },
+  {
+    "revision": "d3363592a6a675ce628e",
+    "url": "/static/js/2.efe8201c.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/static/js/2.efe8201c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a89aaf1fc0ef6344b89",
+    "url": "/static/js/main.89c96d00.chunk.js"
+  },
+  {
+    "revision": "553521c6f998c5ab6b3c",
+    "url": "/static/js/runtime-main.a1e57638.js"
+  },
+  {
+    "revision": "e89b2978f5b0440b60d745df2cf412f9",
+    "url": "/static/media/left-arrow.e89b2978.svg"
+  },
+  {
+    "revision": "efe92f5fa22ca816359f7309107d4438",
+    "url": "/static/media/logout.efe92f5f.svg"
+  },
+  {
+    "revision": "cb565ec6e184dc6faef8472fa6c5b53f",
+    "url": "/static/media/menu.cb565ec6.svg"
+  }
+]);
